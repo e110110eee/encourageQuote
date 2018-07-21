4206c795a9f7c55ba6536b42aa9e3455a68d88eb
王邸 <wohenzhuai0713@163.com>
2018-07-21 18:01:26 +0800

    change word maked  up by myself

--- js/main.js
+++ js/main.js
@@ -1,6 +1,3 @@
-/*
-  Code by Gabriel Nunes
-*/
 
 function inIframe () { try { return window.self !== window.top; } catch (e) { return true; } }
 
@@ -19,7 +16,7 @@ function getQuote() {
     url: 'https://andruxnet-random-famous-quotes.p.mashape.com/cat=',
     success: function(r) {
       if (typeof r === 'string') {
-       r = JSON.parse(r); 
+       r = JSON.parse(r);
       }
       if (Array.isArray(r)) {
        r = r[0];
@@ -52,8 +49,8 @@ function getQuote() {
           $('#author').html(r.author);
         });
       $(".quote-box").animate({
-        opacity:1,
-        marginTop:"20%"
+        opacity:1
+        // marginTop:"20%"
       },500);
       $(".footer").animate({
           opacity: 1
@@ -69,12 +66,22 @@ function getQuote() {
     }
   });
 }
+var buffer_index = "";
 $(document).ready(function() {
   $(".quote-box").css("opacity","0");
   $(".footer").css("opacity","0");
+  showEncourage(Phrase_arr);
+  $('#new-quote').on('click', function() {
+    var random_value = Math.random();
+    if (random_value >= 0.5) {
+      showEncourage(Phrase_arr);
+    }
+    else {
+      showEncourage(Phrase_arr);
+      // getQuote1()
+    }
+  });
 
-  getQuote(); 
-  $('#new-quote').on('click', getQuote1);
   // $('#tweet-quote').on('click', function() {
   //   if(!inIframe()) {
   //     openURL('https://twitter.com/intent/tweet?hashtags=quotes&related=freecodecamp&text=' + encodeURIComponent('"' + currentQuote + '" ' + currentAuthor));
@@ -110,7 +117,6 @@ function getQuote1() {
   });
 }
 function showquote(txt) {
-  console.log(txt)
   var chinese = txt[0].chinese;
   var english = txt[0].english;
 
@@ -174,4 +180,66 @@ function formatterDateTime() {
   + (date.getSeconds() < 10 ? "0" + date.getSeconds() : date
   .getSeconds());
   return datetime;
+}
+var Phrase_arr =
+[
+"少一点功利主义的追求,多一点不为什么的坚持。",
+"你的对手都六点起了，那你呢？",
+"未来的你一定会感激现在不断努力进步的自己",
+"为梦想灼伤了自己，也不愿平庸的喘息",
+"只有经历地狱般的磨难，才能炼出创造天堂的力量；只有流过血的手指，才能弹奏出世间的绝唱。—— 泰戈尔",
+"当你拼尽全力全力也战胜不了躁动的内心时，告诉自己再坚持一分钟",
+"保持健康，保持自信，保持微笑",
+"今晚学到两点，如果你愿意的话"
+
+]
+
+function showEncourage (phrase) {
+  var encourageWords = phrase;
+  var len = encourageWords.length;
+  var i = len * Math.random();
+  if (buffer_index == Math.floor(i)) {
+    //注意return 迭代不return 还会执行之前的外层调用的
+    return showEncourage(Phrase_arr);
+
+  } else {
+    $(".quote-text").animate({
+        opacity: 0
+      }, 500,
+      function() {
+        $(this).animate({
+          opacity: 1
+        }, 500);
+        $(".fa_quote").addClass("fa fa-quote-left")
+        $('#text').text(encourageWords[Math.floor(i)]);
+      });
+
+    $(".quote-chinese").animate({
+        opacity: 0
+      }, 500,
+      function() {
+        $(this).animate({
+          opacity: 1
+        }, 500);
+        $('#chinese').text("献给还在奋斗的你");
+        $(".quote-box").animate({
+          opacity:1,
+          marginTop:"20%"
+        },500);
+        $(".footer").animate({
+            opacity: 1
+          }, 500);
+      });
+
+
+    var color = Math.floor(Math.random() * colors.length);
+    $("html body").animate({
+      backgroundColor: colors[color],
+      color: colors[color]
+    }, 1000);
+    $(".button").animate({
+      backgroundColor: colors[color]
+    }, 1000);
+  }
+  buffer_index = Math.floor(i) ;
 }
